f5d21fdb905268fd15bdcdf017bf6e1cdbecda45
Mustaqeem Tariq <mustaqeemtariq66@gmail.com>
2023-07-04 10:23:11 +0500

    add svg

--- src/assets/icons.ts
+++ src/assets/icons.ts
@@ -2,8 +2,10 @@ import image1 from "assets/png/1.jpg"
 import image2 from "assets/png/2.jpg"
 import image3 from "assets/png/3.jpg"
 import image4 from "assets/png/4.jpg"
+import plus from "assets/svg/plus.svg"
 
 export const house = image1
 export const building = image2
 export const apartment = image3
 export const villa = image4
+export const plusIcon = plus
